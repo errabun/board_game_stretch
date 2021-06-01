7f1d86db2438559aff1a54cccd68c7c8520b81e2
Ryan Miller <ryandmiller3521@gmail.com>
2021-05-31 21:10:04 -0600

    Add provider and bring store into the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import logo from './logo.svg';
 import './App.css'; 
-import { createStore } from 'redux';
-import rootReducer from './reducers'
 
-const store = createStore(rootReducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 function App() {
   return (
