94fb133448eadaa5d54d5db3c1c9ee7d3c0081cf
Eric Rabun <errabun@gmail.com>
2021-06-01 08:27:02 -0600

    Install react router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import './Header.css'
+import { Route, NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <nav className="nav-bar"> 
+    <nav className="nav-bar">
       <h1>🎲  Board Game Blitz</h1>
+      <h3>Wishlist</h3>
     </nav>
   )
 }
