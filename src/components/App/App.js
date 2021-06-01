5d6fee986fbf45a5728092e4e1eab607aec12a5b
Ryan Miller <ryandmiller3521@gmail.com>
2021-06-01 10:58:45 -0600

    Add api call for starting board game data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,21 @@
-import { Component } from 'react'
+import React, { useEffect } from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Wishlist from '../Wishlist/Wishlist'
 import { Switch, Route } from 'react-router-dom'
 import './App.css';
+import { getGames } from '../../apiCalls.js';
+import { useSelector, useDispatch } from 'react-redux';
+import { addGames } from '../../actions/index';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      games: []
-    }
-  }
+function App() {
+  const dispatch = useDispatch();
 
-  render() {
+  useEffect(() => {
+    getGames()
+      .then(data => dispatch(addGames(data.games)))
+  }) 
+
     return (
       <main>
         <Header />
@@ -21,7 +23,7 @@ class App extends Component {
         <Footer />
       </main>
     )
-  }
+
 }
 
 export default App;
