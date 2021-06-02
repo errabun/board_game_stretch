64dcd5b3a852905809ad763a110ff71451933ac1
Ryan Miller <ryandmiller3521@gmail.com>
2021-06-02 10:11:10 -0600

    Remove duplicate code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -25,24 +25,6 @@ function App() {
       .catch((error) => console.log(error));
   }, []) 
 
-
-  const games = useSelector(state => state.boardGames)
-
-  const gameImages = games.map(game => {
-    return (
-      <Link to={`/games:${game.id}`} className='game' key={game.id} style={{backgroundImage: `url(${game.image_url})`}}>
-        <header className='game-header'>
-          <h1>{game.name}</h1>
-          <p className='description'>{game.description_preview}</p>
-        </header>
-        <footer className='game-footer'>
-          <p className='price'><em>${game.price}</em></p>
-          <button onClick={() => dispatch(addWish(game.id))}>Add to wishlist</button>
-        </footer>
-      </Link>
-    )
-  })
-
   return (
     <main>
       <Header />
