c9628b85306def5c8a2dbefe049a862e29a24360
Melanie Daoheuang <daoh0031@gmail.com>
2021-06-03 16:57:01 -0600

    Fix video bug

--- src/components/GameDetails/GameDetails.js
+++ src/components/GameDetails/GameDetails.js
@@ -4,61 +4,51 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addWish, addVideos } from '../../actions';
 import { getVideo } from '../../apiCalls.js';
 
-// function GameDetails({id}) {
-//   const [game, setGame] = useState({});
-//   const state = useSelector(state => state.boardGames);
-//   useEffect(() => {
-//       setGame(state.find(game => game.id === id));
-
-//   }, [state])
-
-
-
 function GameDetails({id}) {
-  // const dispatch = useDispatch(); 
+  const dispatch = useDispatch(); 
   const [game, setGame] = useState({});
   const state = useSelector(state => state.boardGames);
   useEffect(() => {
       setGame(state.find(game => game.id === id));
   }, [state])
   
-  const [video, setVideo] = useState('');
-  let videoKey;
-
-  // const game = useSelector(state => state.gameDetails);
-
-  console.log(video)
+  const [videoKey, setVideo] = useState('');
+ 
 
   useEffect(() => {
-    getVideo(game.id)
+    getVideo(id)
       .then(data => { 
+        console.log(data)
         console.log(data.videos[0].url)
-        videoKey = data.videos[0].url.split("?v=")[1];
-        setVideo(videoKey)
+        setVideo(data.videos[0].url.split("?v=")[1])
       })
       .catch((error) => console.log(error));
   },[])
 
-
+console.log(videoKey)
   return(
-    <div> {game !== undefined &&     
+    <div>{game !== undefined &&     
         <div className='game-details'>
-          <img src={game.thumb_url}/>
-          <h1>{game.name}</h1>
-          <p>{game.price}</p>
+          <header>
+            <img src={game.thumb_url}/>
+            <h1>{game.name}</h1>
+          </header>
+          <p>${game.price}</p>
           <button onClick={() => dispatch(addWish(game))}>Add to wish list</button>
         {/* <p>{game.primary_publisher.name)}</p> */}
         <p>{game.description_preview}</p>
-
+      {videoKey && (
         <iframe
-        // src={`https://www.youtube-nocookie.com/embed/${videoKey}`}
-        src="https://www.youtube-nocookie.com/embed/Dfq4dRPHIAM"
+        src={`https://www.youtube-nocookie.com/embed/${videoKey}`}
+        // src="https://www.youtube-nocookie.com/embed/Dfq4dRPHIAM"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
         title="Embedded youtube"
         seamless
         />
+
+      )}
       </div>}
     </div>
   )
