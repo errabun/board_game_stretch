35f6249b1284f879aed31f1b06452621ac7fbd0a
Eric Rabun <errabun@gmail.com>
2021-06-03 18:31:36 -0600

    Add useDispatch to send game to Wishlist from GameDetails

--- src/components/GameDetails/GameDetails.js
+++ src/components/GameDetails/GameDetails.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import './GameDetails.css'; 
-import { useSelector } from 'react-redux';
+import './GameDetails.css';
+import { useSelector, useDispatch } from 'react-redux';
 import rootReducer from '../../reducers'
 import { createStore } from 'redux';
+import { addWish } from '../../actions';
 
 function GameDetails({id}) {
   const [game, setGame] = useState({});
@@ -12,13 +13,15 @@ function GameDetails({id}) {
 
   }, [state])
 
+  const dispatch = useDispatch()
+
   return(
-    <div> {game !== undefined &&     
+    <div> {game !== undefined &&
         <div className='game-details'>
           <img src={game.thumb_url}/>
           <h1>{game.name}</h1>
           <p>{game.price}</p>
-          <button>Add to wish list</button>
+          <button onClick={() => dispatch(addWish(game.id))}>Add to wish list</button>
         {/* <p>{game.primary_publisher.name)}</p> */}
         <p>{game.description_preview}</p>
       </div>}
@@ -28,22 +31,4 @@ function GameDetails({id}) {
 
 export default GameDetails;
 
-// Game's designers/develoipers >> "Objects are not valid as a React child" >> to render a collection of children use array instead? 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Game's designers/develoipers >> "Objects are not valid as a React child" >> to render a collection of children use array instead?
