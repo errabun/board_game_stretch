4948a95b15d0ce3876fbf5adb16419619730b21d
Melanie Daoheuang <daoh0031@gmail.com>
2021-06-05 16:22:41 -0600

    Style mobile footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,11 @@ import { Route, NavLink, Link } from 'react-router-dom'
 const Header = () => {
   return (
     <nav className="nav-bar">
-      <Link className="wishlist_title" to='/wishlist'><i className="far fa-heart"></i>Wishlist</Link>
-      <div>
+      <div className='game-header'>
         <Link to='/'><i className="fas fa-home"></i></Link>
         <a className='game-title'>Board <span className='die'>🎲 </span> Game <span className='die'>🎲 </span> Blitz </a>
       </div>
+      <Link className="wishlist_title" to='/wishlist'><i className="far fa-heart"></i>Wishlist</Link>
     </nav>
   )
 }
